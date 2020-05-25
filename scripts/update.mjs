ffc650b02c6fea2bb38ae28c1b111086bb7ea7a2
Luke Saunders <luke.saunders@gmail.com>
2020-05-26 00:56:30 +0100

    update script works

--- scripts/update.mjs
+++ scripts/update.mjs
@@ -1,54 +1,23 @@
-import { request } from 'graphql-request';
-import { format } from 'date-fns'
+import graphql from 'graphql-request';
+const { request } = graphql;
 
-import knex from '../../lib/database.js';
+import fns from 'date-fns';
+const { format } = fns;
+
+import knex from '../lib/database.mjs';
 const endpoint = 'http://prod.api.graph.plasma.2key.net/subgraphs/name/plasma';
-
-async function metaStats() {
-  const query = `{
-    meta(id:"Meta") {
-      _visitCounter
-      _n_conversions
-      _n_conversions_rejected
-      _conversionsExecuted
-      _visitCounter
-      _n_forwarded
-      _n_campaigns
-    }
-    visits(orderBy:_timeStamp,orderDirection:desc,first:2) {
-      id
-      _campaign {
-        id
-      }
-      _referrer {
-        id
-      }
-    }
-    user(id:"0x5224aa663e2ba21ccb4194bcd4f1aed5f4be4d31") {
-      id
-      _handle
-      _web3Address
-    }
-  }`;
-
-  request('http://prod.api.graph.plasma.2key.net/subgraphs/name/plasma', query).then(data =>
-    console.log(data)
-  );
-};
-
-
 
 async function requestVisits({ after, dateMap = {} } = {}) {
   console.log('fetching after', after);
   const afterSegment = after ?
     `where:{
-      _timeStamp_lt:${after}
+      _timeStamp_gt:${after}
     }` : '';
 
   const query = `{
     visits(
       orderBy:_timeStamp,
-      orderDirection:desc,
+      orderDirection:asc,
       first:100
       ${afterSegment}
     ) {
@@ -71,23 +40,23 @@ async function requestVisits({ after, dateMap = {} } = {}) {
 };
 
 async function run() {
-  const token = await knex('tokens').where({ gecko_id: '2key' }).first();
-  await knex('project_key_metrics').where({ token_id: token.id, metric_name: 'link_visits' }).del();
+  await knex('key_metrics').where({ metric_name: 'link_clickthroughs' }).del();
   const dateMap = await requestVisits();
-  console.log('sdfdsf');
   console.log(dateMap);
 
+  let cumulativeCount = 0;
   for (let [key, value] of Object.entries(dateMap)) {
-    await knex('project_key_metrics').insert({
+    if (key === format(new Date(), 'yyyy-MM-dd')) continue;
+    cumulativeCount += value;
+    await knex('key_metrics').insert({
       date: key,
-      token_id: token.id,
-      metric_name: 'link_visits',
-      metric_count: value,
+      metric_name: 'link_clickthroughs',
+      daily_count: value,
+      cumulative_count: cumulativeCount,
     });  
   }
+
+  knex.destroy();
 }
 
 run();
-
-
-
